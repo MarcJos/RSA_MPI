1f7598b89d71f921833bced31c22f95e87f31591
rprat-pro <rp269144@pleiades1217.intra.cea.fr>
2025-01-19 13:00:18 +0100

    update spack branch

--- spack_repo/packages/rsampi/package.py
+++ spack_repo/packages/rsampi/package.py
@@ -7,7 +7,7 @@ class Rsampi(CMakePackage):
     homepage = "https://github.com/MarcJos/RSA_MPI"
     git = "https://github.com/MarcJos/RSA_MPI.git"
 
-    version("1.0", commit="3caa41dd77c1880802e9f38e6f4399b380ea995d")
+    version('0.1.0', git='https://github.com/MarcJos/RSA_MPI.git', branch='v0.1.0')
 
     depends_on("cmake@3.26.3")
     depends_on("openmpi")
